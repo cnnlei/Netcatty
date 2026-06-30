433927fcfe55c1f77efcd2842b81d5244870aaee
bincxz <16399091+binaricat@users.noreply.github.com>
2026-06-30 17:27:34 +0800

    Add Nix AppImage package

---
+++ nix/package.nix
@@ -0,0 +1,63 @@
+{
+  appimageTools,
+  fetchurl,
+  lib,
+  stdenv,
+}:
+
+let
+  pname = "netcatty";
+  version = "1.1.51";
+
+  sources = {
+    x86_64-linux = {
+      appImageArch = "x86_64";
+      hash = "sha256-xMTXP3gjBuAzDXIYuhbQsCkwcf9lDMnRvqkL01PbGTA=";
+    };
+    aarch64-linux = {
+      appImageArch = "arm64";
+      hash = "sha256-UyWRFuc1yN2CkUl0/jyJ3+lJkrpvGHZB0u5itJ0CkG4=";
+    };
+  };
+
+  source =
+    sources.${stdenv.hostPlatform.system}
+      or (throw "Netcatty AppImage packages are available for x86_64-linux and aarch64-linux only.");
+
+  src = fetchurl {
+    url = "https://github.com/binaricat/Netcatty/releases/download/v${version}/Netcatty-${version}-linux-${source.appImageArch}.AppImage";
+    inherit (source) hash;
+  };
+
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    desktopSource="$(find ${appimageContents} -maxdepth 3 -name '*.desktop' -print -quit)"
+    if [ -n "$desktopSource" ]; then
+      desktopFile="$out/share/applications/netcatty.desktop"
+      install -Dm444 "$desktopSource" "$desktopFile"
+      substituteInPlace "$desktopFile" \
+        --replace "Exec=AppRun" "Exec=netcatty" \
+        --replace "Exec=Netcatty" "Exec=netcatty"
+    fi
+
+    if [ -d "${appimageContents}/usr/share/icons" ]; then
+      mkdir -p "$out/share"
+      cp -r "${appimageContents}/usr/share/icons" "$out/share/"
+    fi
+  '';
+
+  meta = {
+    description = "Modern SSH client and terminal manager";
+    homepage = "https://github.com/binaricat/Netcatty";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "netcatty";
+    platforms = builtins.attrNames sources;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+  };
+}
